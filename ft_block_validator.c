04dbf6702d435b9917669b73af00934b42c3ac65
Dimitri LAVAURY-COLLOT <dlavaury@e1r8p13.42.fr>
2017-11-16 18:46:11 +0100

    fixes

--- ft_block_validator.c
+++ ft_block_validator.c
@@ -6,7 +6,7 @@
 /*   By: dlavaury <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/13 17:15:32 by dlavaury          #+#    #+#             */
-/*   Updated: 2017/11/15 19:23:12 by dlavaury         ###   ########.fr       */
+/*   Updated: 2017/11/16 18:45:33 by dlavaury         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,9 @@
 void	ft_block_validator(t_data *data, int i)
 {
 	data->error = 1;
-	if (data->nb_b < 3
-	&& ((i + 1 < 19 && data->buff[i + 1] == '#')
+	if ((i + 1 < 19 && data->buff[i + 1] == '#')
 	|| (i + 5 < 19 && data->buff[i + 5] == '#')
-	|| (i + 4 < 19 && data->buff[i + 4] == '#')
-	|| (i + 3 < 19 && data->buff[i + 3] == '#')))
-		data->error = 0;
-	else if (data->nb_b == 3
-	&& ((data->buff[i - 1] == '#')
-	|| (data->buff[i - 5] == '#')))
+	|| (i - 1 >= 0 && data->buff[i - 1] == '#')
+	|| (i - 5 >= 0 && data->buff[i - 5] == '#'))
 		data->error = 0;
 }
