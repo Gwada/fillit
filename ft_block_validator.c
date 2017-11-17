ec030de0a1212f621fb0c49cc0c26659e4bf66a4
Dimitri LAVAURY-COLLOT <dlavaury@e3r11p10.42.fr>
2017-11-17 12:58:01 +0100

    fixes

--- ft_block_validator.c
+++ ft_block_validator.c
@@ -6,33 +6,61 @@
 /*   By: dlavaury <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/13 17:15:32 by dlavaury          #+#    #+#             */
-/*   Updated: 2017/11/17 11:24:30 by dlavaury         ###   ########.fr       */
+/*   Updated: 2017/11/17 12:57:26 by dlavaury         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int		ft_block_validator(t_data *data, int i)
+static void	ft_first_block(t_data *data, int i)
+{
+	if ((i + 1 < 18 && data->buff[i + 1] == '#')
+	|| (i + 5 < 19 && data->buff[i + 5] == '#'))
+		data->error = 0;
+}
+
+static void	ft_second_block(t_data *data, int i)
+{
+	if (((i - 1 >= 0 && data->buff[i - 1] == '#')
+	|| (i - 3 > 1 && data->buff[i - 3] == '#')
+	|| (i - 4 && data->buff[i - 4] == '#')
+	|| (i - 5 >= 0 && data->buff[i - 5] == '#'))
+	&& ((i + 1 < 18 && data->buff[i + 1] == '#')
+	|| (i + 3 < 17 && data->buff[i + 3] == '#')
+	|| (i + 4 < 18 && data->buff[i + 4] == '#')
+	|| (i + 5 < 19 && data->buff[i + 5] == '#')))
+		data->error = 0;
+}
+
+static void	ft_third_block(t_data *data, int i)
+{
+	if (((i - 1 && data->buff[i - 1] == '#')
+	|| (i - 4 && data->buff[i - 4] == '#')
+	|| (i - 5 >= 0 && data->buff[i - 5] == '#'))
+	&& ((i + 1 < 19 && data->buff[i + 1] == '#')
+	|| (i + 3 < 17 && data->buff[i + 3] == '#')
+	|| (i + 4 < 18 && data->buff[i + 4] == '#')
+	|| (i + 5 < 19 && data->buff[i + 5] == '#')))
+		data->error = 0;
+}
+
+static void	ft_last_block(t_data *data, int i)
+{
+	if ((i - 1 > 1 && data->buff[i - 1] == '#')
+	|| (i - 5 >= 0 && data->buff[i - 5] == '#'))
+		data->error = 0;
+}
+
+int			ft_block_validator(t_data *data, int i)
 {
 	data->error = 1;
-	if (!data->nb_b && ((i + 1 < 18 && data->buff[i + 1] == '#')
-		|| (i + 5 < 17 && data->buff[i + 5] == '#')))
-		data->error = 0;
-	else if (data->nb_b == 1 && (((i - 1 >= 0 && data->buff[i - 1] == '#')
-		|| (i - 5 >= 0 && data->buff[i - 1] == '#'))
-		&& ((i + 1 < 19 && data->buff[i - 1] == '#')
-		|| (i + 4 < 19 && data->buff[i + 4] == '#')
-		|| (i + 5 < 19 && data->buff[i + 5] == '#'))))
-		data->error = 0;
-	else if (data->nb_b == 2 && (((i - 1 >= 1 && data->buff[i - 1] == '#')
-		|| (i - 5 >= 0 && data->buff[i - 5] == '#'))
-		&& ((i + 1 < 20 && data->buff[i + 1] == '#')
-		|| (i + 3 < 18 && data->buff[i + 3] == '#')
-		|| (i + 4 < 19 && data->buff[i + 4] == '#')
-		|| (i + 5 < 20 && data->buff[i + 5] == '#'))))
-		data->error = 0;
-	else if ((i - 1 >= 2 && data->buff[i - 1] == '#')
-		|| (i - 5 >= 0 && data->buff[i - 5] == '#'))
-		data->error = 0;
+	if (!data->nb_b)
+		ft_first_block(data, i);
+	else if (data->nb_b == 1)
+		ft_second_block(data, i);
+	else if (data->nb_b == 2)
+		ft_third_block(data, i);
+	else
+		ft_last_block(data, i);
 	return (data->error ? 0 : 1);
 }
